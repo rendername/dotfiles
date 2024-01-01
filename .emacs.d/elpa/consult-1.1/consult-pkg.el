511c98a11ace0ba3f852ec3e156f42dc6225c3b0
anthony erbe <anthony.erbe@gmail.com>
2024-01-01 00:59:26 -0600

    wip

---
+++ .emacs.d/elpa/consult-1.1/consult-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from consult.el  -*- no-byte-compile: t -*-
+(define-package "consult" "1.1" "Consulting completing-read" '((emacs "27.1") (compat "29.1.4.4")) :commit "0e7f3ec8e7e959e262158fa4ddb73de80b52ccc0" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
