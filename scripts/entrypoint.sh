a984f7718770c6c7280f1bc4492c85ae9e338616
Anthony Erbe <anthony.erbe@orangelogic.com>
2024-01-31 02:00:59 -0600

    wip

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@ for f in $(find "$SCRIPTS_PATH/user" -name "*.sh" -type f); do
     source "$f"
 done
 
-if [ -d "/home/anthony/.scripts-local" ]; then
+if [ -d "/home/anthony/.scripts-local/" ]; then
     for f in $(find "/home/anthony/.scripts-local" -name "*.sh" -type f); do
         source "$f"
     done
